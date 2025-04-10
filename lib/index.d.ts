30335be0f2711ea31fdc53c49ca114cd381c7a4b
Nika Shamiladze <shamgoingzuck@gmail.com>
2025-04-10 22:17:22 +0400

    feat: change the name and changed some files content in lib folder

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,15 +7,24 @@ declare abstract class ModuleBase extends SimpleModuleBase {
     constructor(faaker: Faaker);
 }
 
+declare enum Sex {
+    Female = "female",
+    Male = "male"
+}
+type SexType = `${Sex}`;
 declare class PersonModule extends ModuleBase {
+    private readonly mapTheArray;
     private readonly selectDefinition;
-    firstName(): string;
+    firstName(sex?: SexType): string;
+    lastName(sex?: SexType): string;
+    middleName(sex?: SexType): string;
+    fullName(sex?: SexType): string;
+    prefix(sex?: SexType): string;
+    gender(sex?: SexType): string;
 }
 
 declare class Faaker {
     readonly person: PersonModule;
 }
 
-declare const faaker: Faaker;
-
-export { faaker };
+export { Faaker };
