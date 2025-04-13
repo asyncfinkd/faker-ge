616cc46aea6048028dd252941b2bc76765574109
Nika Shamiladze <shamgoingzuck@gmail.com>
2025-04-13 15:34:05 +0400

    refactor: remove unused compress and lazy load utility functions

--- src/utils/compress.ts
+++
@@ -1,7 +0,0 @@
-export const compressArray = <T>(arr: T[]): string => {
-  return JSON.stringify(arr);
-};
-
-export const decompressArray = <T>(compressed: string): T[] => {
-  return JSON.parse(compressed);
-};
